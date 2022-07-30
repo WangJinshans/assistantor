581e9bdf6969a43a703218093cda0a8d3e426366
wang jinshan <wangjinshan231@gmail.com>
2022-07-31 01:25:54 +0800

    fix: 修正role api没有code返回的问题

--- api/role/role.go
+++ api/role/role.go
@@ -1,6 +1,7 @@
 package role
 
 import (
+	"assistantor/common"
 	"assistantor/global"
 	"github.com/gin-gonic/gin"
 )
@@ -14,38 +15,43 @@ func (*ApiRole) GetAllRoles(ctx *gin.Context) {
 	enforcer := global.GetEnforcer()
 	roleList := enforcer.GetAllRoles()
 	ctx.JSON(200, gin.H{
+		"code":    common.Success,
 		"message": roleList,
 	})
 	return
 }
 
-
 func (*ApiRole) AddRoleForUser(ctx *gin.Context) {
 	enforcer := global.GetEnforcer()
 	userId, ok := ctx.Params.Get("user_id")
-	if !ok{
+	if !ok {
 		ctx.JSON(200, gin.H{
+			"code":    common.Fail,
 			"message": "",
 		})
 	}
 	role, ok := ctx.Params.Get("role")
-	if !ok{
+	if !ok {
 		ctx.JSON(200, gin.H{
+			"code":    common.Fail,
 			"message": "",
 		})
 	}
 	result, err := enforcer.AddRoleForUser(userId, role)
-	if err!=nil{
+	if err != nil {
 		ctx.JSON(200, gin.H{
+			"code":    common.Fail,
 			"message": "",
 		})
 	}
-	if !result{
+	if !result {
 		ctx.JSON(200, gin.H{
+			"code":    common.Fail,
 			"message": "",
 		})
 	}
 	ctx.JSON(200, gin.H{
+		"code":    common.Success,
 		"message": "success",
 	})
 	return
