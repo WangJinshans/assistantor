10cd69034b148433a8f2ce4177a826e71733695b
wang jinshan <wangjinshan231@gmail.com>
2022-08-02 22:12:55 +0800

    feat: 新增orm sql测试

--- debug/orm_test.go
+++ debug/orm_test.go
@@ -10,18 +10,17 @@ import (
 
 type User struct {
 	gorm.Model
-	Name string `gorm:"index" json:"name"`
+	Name   string `gorm:"index" json:"name"`
+	UserID string
 	// 使用默认外键在写入User的时候默认会把User表中的ID字段写入到Company表中的UserID字段
 	// 可使用foreignKey,references指定关联关系, foreignKey:UserName;references:name 会将User表中的name字段写到Company表中的UserName字段
 	// foreignKey是其他表字段, references是本表字段
-	Company []Company `gorm:"foreignKey:UserName;references:name"`
+	Cid uint
 }
 type Company struct {
 	gorm.Model
-	ID       int    `json:"ID"`
-	UserName string `gorm:"size:10"`
-	Name     string `json:"name"`
-	CSn      string `json:"cSn"`
+	Name string `json:"name"`
+	CSn  string `json:"cSn"`
 }
 
 func TestGormFilePartition(t *testing.T) {
@@ -58,37 +57,32 @@ func TestGormFilePartition(t *testing.T) {
 
 func TestGorm(t *testing.T) {
 
-	engine, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+	engine, err := gorm.Open(sqlite.Open("./gorm.db"), &gorm.Config{})
 	if err != nil {
 		log.Info().Msgf("create db error: %v", err)
 		return
 	}
 	engine.AutoMigrate(&User{}, &Company{})
-	c := Company{
-		ID:   1,
-		Name: "cnm",
-	}
-	c2 := Company{
-		ID:   2,
-		Name: "cnm2",
-	}
-
-	u := User{
-		Name: "lance",
-		Company: []Company{
-			c, c2,
-		},
-	}
-
+	//c := Company{
+	//	Name: "cnm",
+	//}
+	//c.ID = 1
+	//c2 := Company{
+	//	Name: "cnm2",
+	//}
+	//c2.ID = 2
+	//
+	//u := User{
+	//	Name: "lance",
+	//	Cid: c.ID,
+	//}
+	//
 	//engine.Save(&c)
 	//engine.Save(&c2)
 	//engine.Save(&u)
 
-	var user User
-	engine.Model(&User{}).Preload("Company").Where("name = ?", "lance").First(&user)
-	log.Info().Msgf("user is: %v", user)
 
-	var companyList []Company
-	engine.Model(&u).Association("Company").Find(&companyList)
-	log.Info().Msgf("company list is: %v", companyList)
+	var u User
+	err = engine.Table("companies").Select("users.*").Joins("left join users on companies.id = users.cid").Where("companies.id = ?", 1).Find(&u).Error
+	log.Info().Msgf("user is: %v, error is: %v", u, err)
 }
